9e155df947d0276eb5111494649efba76d1fb452
DESKTOP-7V9Q99B\AKU <akuraza6atgmail.com>
2023-08-16 21:16:57 +0500

    add 4

--- code.js
+++ code.js
@@ -7,8 +7,20 @@
 
 // check object is an array or not rovide some code
 
-const arr=[]
-const obj={}
-console.log(Array.isArray(arr))
-console.log(Array.isArray(obj))
+// const arr=[]
+// const obj={}
+// console.log(Array.isArray(arr))
+// console.log(Array.isArray(obj))
+
+// How to empty an array in javascript
+
+// const arr=[3,4,5,10]
+// arr.length=0
+// console.log(arr)
+
+// How to check the number is an integer
+const numb= 10.5;
+console.log(typeof numb)
+console.log(Number.isInteger(numb))
+
 
