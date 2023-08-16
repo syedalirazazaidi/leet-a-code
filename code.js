4c66d94a58b121f429b8f42977479e676eacc74e
DESKTOP-7V9Q99B\AKU <akuraza6atgmail.com>
2023-08-16 21:12:29 +0500

    add 2

---
+++ code.js
@@ -0,0 +1,14 @@
+// reverse each words in a row
+
+// const str='hi this is raza'
+
+// const newstrmy=str.split(' ').map((strnw)=>(strnw.split('').reverse().join(''))).join(' ')
+// console.log(newstrmy)
+
+// check object is an array or not rovide some code
+
+const arr=[]
+const obj={}
+console.log(Array.isArray(arr))
+console.log(Array.isArray(obj))
+
